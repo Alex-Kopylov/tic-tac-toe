a2035ce24d9607732da880e7aea3ee46addf533f
Alex <alexkopylov123@gmail.com>
2019-04-22 21:55:36 +0400

    need to handle thread

--- tic-tac-toe/Cell.h
+++ tic-tac-toe/Cell.h
@@ -5,7 +5,7 @@ public:
 	Cell();
 	static int get_cell_number_from_point(int x_pos, int y_pos, RECT board_rect);
 	~Cell();
-	static const RECT * get_cell_rectangle(int index, RECT board_rect);
+	RECT * get_cell_rectangle(int index, RECT board_rect);
 	HICON getIcon(int player_turn);
 	void delete_brushes() const;
 	void mark_the_cell(HDC hdc, int player_turn, int x_left, int y_top);
--- tic-tac-toe/Game.h
+++ tic-tac-toe/Game.h
@@ -4,6 +4,7 @@
 #include <random> 
 #include <ctime>
 #include <fstream>
+#include <cstdlib>
 class Game
 {
 public:
@@ -13,17 +14,15 @@ public:
 	bool is_this_cell_empty(int index);
 	std::vector<unsigned int> get_game_board() const;
 	bool is_game_over();
-	void start_new_game(unsigned int game_number);
+	void start_new_game(const unsigned short int game_number);
 	void auto_step();
 	unsigned int get_player_turn() const;
 	unsigned short* get_game_stat();
 	~Game();
 private:
-	unsigned short int game_number_;
-	static  std::ofstream file_;
 	unsigned short int player_turn_ = 1;
 	std::vector<unsigned int> gameboard_ =  std::vector<unsigned int>(9,0);
-	std::vector<unsigned short int> win_conditions = { 0,1,2, //24 elements
+	std::vector<unsigned short int> win_conditions_ = { 0,1,2, //24 elements
 						3,4,5,
 						6,7,8,
 						0,3,6,
@@ -32,10 +31,17 @@ private:
 						0,4,8,
 						2,4,6, };
 	unsigned short int game_stat_[3] = { 0,0,0 };
-	void keep_game_board_and_start_thread(const unsigned short int winner) const;
+	void keep_game_board_and_start_thread(const unsigned short int winner);
 	void random_step();
 	std::mt19937 gen_{ std::random_device()() };
-	static DWORD WINAPI write_data_in_file(void * lpvoid);
-	typedef struct buffer;
+	static DWORD WINAPI write_data_in_file(PVOID lparam);
+
+	struct myStruct {
+		std::vector<unsigned int> gameboard_;
+		unsigned short int buffer_winner;
+		unsigned short int game_number_;
+	};
+	myStruct* mystruct = new myStruct();
+	HANDLE thread_for_writing_in_file, mutex;
 };
 
